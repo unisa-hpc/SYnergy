bd406c96d423706452497d0f93b1675ba18b0a14
emdant <30555419+emdant@users.noreply.github.com>
2023-09-12 15:23:19 +0200

    Refactor to Level Zero wrapper to be stateless

--- include/device_impl.hpp
+++ include/device_impl.hpp
@@ -45,43 +45,43 @@ public:
     current_uncore_frequency = library.get_uncore_frequency(handle);
   }
 
-  inline virtual ~vendor_device() { library.shutdown(); }
+  inline ~vendor_device() { library.shutdown(); }
 
-  inline virtual std::vector<frequency> supported_core_frequencies() { return library.get_supported_core_frequencies(handle); }
+  inline std::vector<frequency> supported_core_frequencies() { return library.get_supported_core_frequencies(handle); }
 
-  inline virtual std::vector<frequency> supported_uncore_frequencies() { return library.get_supported_uncore_frequencies(handle); }
+  inline std::vector<frequency> supported_uncore_frequencies() { return library.get_supported_uncore_frequencies(handle); }
 
-  inline virtual frequency get_core_frequency(bool cached = true) { return cached ? current_core_frequency : library.get_core_frequency(handle); }
+  inline frequency get_core_frequency(bool cached = true) { return cached ? current_core_frequency : library.get_core_frequency(handle); }
 
-  inline virtual frequency get_uncore_frequency(bool cached = true) { return cached ? current_uncore_frequency : library.get_uncore_frequency(handle); }
+  inline frequency get_uncore_frequency(bool cached = true) { return cached ? current_uncore_frequency : library.get_uncore_frequency(handle); }
 
-  inline virtual void set_core_frequency(frequency target) {
+  inline void set_core_frequency(frequency target) {
     library.set_core_frequency(handle, target);
     current_core_frequency = target;
     current_uncore_frequency = library.get_uncore_frequency(handle);
   }
 
-  inline virtual void set_uncore_frequency(frequency target) {
+  inline void set_uncore_frequency(frequency target) {
     library.set_uncore_frequency(handle, target);
     current_uncore_frequency = target;
     current_core_frequency = library.get_core_frequency(handle);
   }
 
-  inline virtual void set_all_frequencies(frequency core, frequency uncore) {
+  inline void set_all_frequencies(frequency core, frequency uncore) {
     library.set_all_frequencies(handle, core, uncore);
     current_core_frequency = core;
     current_uncore_frequency = uncore;
   }
 
-  inline virtual power get_power_usage() {
+  inline power get_power_usage() {
     return library.get_power_usage(handle);
   }
 
-  inline virtual energy get_energy_usage() {
+  inline energy get_energy_usage() {
     return library.get_energy_usage(handle);
   }
 
-  inline virtual unsigned get_power_sampling_rate() {
+  inline unsigned get_power_sampling_rate() {
     return vendor::sampling_rate;
   }
 
