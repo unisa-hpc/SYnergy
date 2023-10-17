d3d00ebcfb9ee4058534e1910c642eda1cf46dde
antonio-decaro <antonio.decaro99@outlook.it>
2023-10-17 12:27:15 +0200

    inserted support for numa

--- include/host_profiler.hpp
+++ include/host_profiler.hpp
@@ -1,31 +1,90 @@
+/**
+ * @file host_profiler.hpp
+ * @brief Host profiler implementation
+ * @details Implementation of the host profiler functions. The profiler uses the Powercap interface
+ * to get the energy consumption of the host. To access the profiler file, root privileges are
+ * required. The host profiler is only available on Linux.
+*/
+
 #include <fstream>
 #include <istream>
+#include <vector>
+#include <filesystem>
 #include <unistd.h>
 
 namespace synergy {
+namespace host_profiler {
 namespace detail {
+
+  constexpr auto POWERCAP_ROOT_DIR = "/sys/class/powercap/";
+  constexpr auto POWERCAP_ENERGY_FILE = "energy_uj";
+  constexpr auto POWERCAP_UNCORE_NAME = "dram";
+  constexpr auto POWERCAP_CORE_NAME = "core";
+  constexpr auto POWERCAP_PACKAGE_NAME = "package";
   
-  void do_root() {
-    setreuid(0, 0);
-  }
+  inline void do_root() { setreuid(0, 0); }
 
-  void undo_root() {
-    setreuid(geteuid(), getuid());
-  }
+  inline void undo_root() { setreuid(geteuid(), getuid()); }
 
-  double get_host_energy() {
-    unsigned long long e;
+  /**
+   * @brief Get the names of the host's packages
+   * @param base_path The base path of the Powercap interface
+   * @return A vector containing the names
+  */
+  std::vector<std::string> get_packages(std::string base_path = POWERCAP_ROOT_DIR) {
+    std::vector<std::string> packages;
+    std::filesystem::path path(base_path);
 
-    do_root();
-    std::ifstream file("/sys/class/powercap/intel-rapl/intel-rapl:0/energy_uj", std::ios::in);
-    if (!file.is_open()) {
-      throw std::runtime_error("synergy::device error: could not open energy file");
+    for (const auto& entry : std::filesystem::directory_iterator(path)) {
+      if (entry.is_directory()) {
+        std::string name = entry.path().filename().string();
+        if (name.find(":") == name.length() - 2) {
+          packages.push_back(name);
+        }
+      }
     }
 
-    file >> e;
-    undo_root();
+    return packages;
+  }
 
-    return static_cast<double>(e);
+  template <typename... Args>
+  std::string build_path(Args... args) {
+    std::string path;
+    ((path += args) + ...); // fold expression to concatenate the strings
+    return path;
   }
 } // namespace detail
+  using namespace detail;
+  
+  /**
+   * @brief Get the energy consumption of the host in microjoules
+   * @details Get the energy consumption of the host in microjoules. The function uses the Powercap
+   * interface to get the energy consumption of the host.
+   * @return A monotonically increasing value representing the energy consumption of the host in
+   * microjoules
+   * @throws std::runtime_error if the energy file(s) cannot be opened
+  */
+  double get_host_energy() {
+    double energy = 0;
+    unsigned long long e;
+
+    do_root(); // we need root privileges to access the energy files
+
+    // if it's a multi-cpu architecture, we want to sum the energy of all the cpus
+    for (const auto& p : get_packages()) {
+      std::string path = build_path(POWERCAP_ROOT_DIR, "/", p, "/", POWERCAP_ENERGY_FILE);
+      std::ifstream file {path, std::ios::in};
+      if (!file.is_open()) {
+        throw std::runtime_error("synergy::device error: could not open energy file");
+      }
+      file >> e;
+      energy += static_cast<double>(e);
+    }
+
+    undo_root(); // reset privileges
+
+    return energy;
+  }
+
+} // namespace host_profiler
 } // namespace synergy
