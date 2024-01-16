0415ba284462014a577e6def099bb9ff40087089
antonio-decaro <antonio.decaro99@outlook.it>
2024-01-16 23:18:29 +0100

    Add belated_kernel class to phase_aware namespace

---
+++ include/phase_aware/belated_kernel.hpp
@@ -0,0 +1,136 @@
+#pragma once
+
+#include <sycl/sycl.hpp>
+#include <stdexcept>
+#include <map>
+#include "../types.hpp"
+
+
+namespace synergy {
+inline namespace v1 {
+namespace detail {
+
+/**
+ * @brief Energy consumption
+*/
+struct consumption_t {
+  frequency frequency;
+  energy energy;
+  time time;
+};
+
+/**
+ * @brief Kernel information
+*/
+class belated_kernel {
+protected:
+  std::map<frequency, consumption_t> core_freq_predictions; // target core frequency -> predicted energy consumption
+  frequency best_core_frequency; // predicted core frequency that satisfies target metric
+  frequency setted_core_frequency; // actual core frequency
+public:
+  std::function<void(sycl::handler&)> cgh; // command group handler
+  belated_kernel(std::function<void(sycl::handler&)> cgh) : cgh{cgh} {}
+
+  inline void add_core_freq_prediction(frequency frequency, consumption_t consumption) noexcept { core_freq_predictions[frequency] = consumption; }
+  inline void add_core_freq_prediction(frequency frequency, energy energy, time time) noexcept { core_freq_predictions[frequency] = consumption_t{frequency, energy, time}; }
+  inline void del_core_freq_prediction(frequency frequency) { core_freq_predictions.erase(frequency); }
+  inline consumption_t get_core_freq_prediction(frequency frequency) const { return core_freq_predictions.at(frequency); }
+  inline void set_best_core_frequency(frequency best_core_frequency) { this->best_core_frequency = best_core_frequency; }
+  inline void set_actual_core_frequency(frequency setted_core_frequency) { this->setted_core_frequency = setted_core_frequency; }
+  inline const std::map<frequency, consumption_t>& get_core_predictions() const noexcept { return core_freq_predictions; }
+  inline frequency get_best_core_frequency() const noexcept { return best_core_frequency; }
+  inline frequency get_actual_core_frequency() const noexcept { return setted_core_frequency; }
+  
+  /**
+   * @brief Calculate the time speedup for a given frequency
+   * @details This function is used to calculate the time speedup between a certain core frequency and the currently setted core frequency
+   * @param freq Frequency to calculate the time speedup
+   * @return A value representing the time speedup
+   * @throw std::runtime_error if the core frequency prediction is not found
+  */
+  inline float calculate_time_speedup(frequency freq) const {
+    try {
+      auto curr_time = core_freq_predictions.at(setted_core_frequency).time;
+      auto new_time = core_freq_predictions.at(freq).time;
+
+      return curr_time / new_time;
+
+    } catch (const std::out_of_range& e) {
+      throw std::runtime_error("synergy::belated_kernel error: core frequency prediction not found");
+    };
+  }
+
+  /**
+   * @brief Calculate the energy saving for a given frequency
+   * @details This function is used to calculate the energy saving between a certain core frequency and the currently setted core frequency
+   * @param freq Frequency to calculate the energy speedup
+   * @return A float value representing the energy saving: if the value is positive, the energy consumption is lower than the currently setted core frequency, otherwise it is higher
+   * @throw std::runtime_error if the core frequency prediction is not found
+  */
+  inline float calculate_energy_saving(frequency freq) const {
+    try {
+      auto curr_energy = core_freq_predictions.at(setted_core_frequency).energy;
+      auto new_energy = core_freq_predictions.at(freq).energy;
+
+      return curr_energy - new_energy;
+
+    } catch (const std::out_of_range& e) {
+      throw std::runtime_error("synergy::belated_kernel error: core frequency prediction not found");
+    };
+  }
+
+  /**
+   * Calculates the cost associated with a given frequency.
+   *
+   * @param freq The frequency for which to calculate the cost.
+   * @return The cost associated with the given frequency.
+   */
+  inline float get_cost(frequency freq, target_metric target) {
+    auto curr_freq = this->get_actual_core_frequency();
+    this->set_actual_core_frequency(freq);
+    try {
+      auto cost = this->get_cost(target);
+      this->set_actual_core_frequency(curr_freq);
+      return cost;
+    } catch (const std::out_of_range& e) {
+      this->set_actual_core_frequency(curr_freq);
+      throw std::runtime_error("synergy::belated_kernel error: core frequency prediction not found");
+    }
+  }
+
+  /**
+   * @brief Returns the cost associated with the kernel.
+   *
+   * @return The cost of the kernel.
+   */
+  inline float get_cost(target_metric target) {
+    switch (target) {
+      case target_metric::EDP:
+        return core_freq_predictions.at(setted_core_frequency).energy * core_freq_predictions.at(setted_core_frequency).time;
+      case target_metric::ED2P:
+        return core_freq_predictions.at(setted_core_frequency).energy * core_freq_predictions.at(setted_core_frequency).time * core_freq_predictions.at(setted_core_frequency).time;
+      case target_metric::ES_25:
+        return calculate_energy_saving(setted_core_frequency) / core_freq_predictions.at(setted_core_frequency).energy;
+      case target_metric::ES_50:
+        return calculate_energy_saving(setted_core_frequency) / core_freq_predictions.at(setted_core_frequency).energy;
+      case target_metric::ES_75:
+        return calculate_energy_saving(setted_core_frequency) / core_freq_predictions.at(setted_core_frequency).energy;
+      case target_metric::ES_100:
+        return calculate_energy_saving(setted_core_frequency) / core_freq_predictions.at(setted_core_frequency).energy;
+      case target_metric::PL_00:
+        return calculate_time_speedup(setted_core_frequency) / core_freq_predictions.at(setted_core_frequency).time;
+      case target_metric::PL_25:
+        return calculate_time_speedup(setted_core_frequency) / core_freq_predictions.at(setted_core_frequency).time;
+      case target_metric::PL_50:
+        return calculate_time_speedup(setted_core_frequency) / core_freq_predictions.at(setted_core_frequency).time;
+      case target_metric::PL_75:
+        return calculate_time_speedup(setted_core_frequency) / core_freq_predictions.at(setted_core_frequency).time;
+      default:
+        return 0;
+    }
+  }
+};
+
+} // namespace detail
+} // namespace v1
+} // namespace synergy
--- include/types.hpp
+++ include/types.hpp
@@ -4,5 +4,20 @@ namespace synergy {
 using frequency = unsigned;
 using power = unsigned long long;
 using energy = double;
+using time = double; // microseconds
+
+enum target_metric {
+  UNDEFINED, // Undefined
+  EDP, // Energy Delay Product
+  ED2P, // Energy Delay Squared Product
+  ES_25, // Energy Saving 25%
+  ES_50, // Energy Saving 50%
+  ES_75, // Energy Saving 75%
+  ES_100, // Energy Saving 100%
+  PL_00, // Performance Loss 0%
+  PL_25, // Performance Loss 25%
+  PL_50, // Performance Loss 50%
+  PL_75, // Performance Loss 75%
+};
 
 } // namespace synergy
