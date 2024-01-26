737b3f8fb334669da47253a3534fa73abc2353f5
Antonio De Caro <antonio.decaro99@outlook.it>
2024-01-26 14:57:38 +0000

    Refactor matrix_mul function and update plot.py

--- samples/freq_overhead/freq_overhead.cpp
+++ samples/freq_overhead/freq_overhead.cpp
@@ -1,38 +1,41 @@
 #include <synergy.hpp>
 #include <vector>
+#include <numeric>
 
 constexpr size_t N = 1024;
 constexpr size_t NUM_RUNS = 10;
+constexpr size_t NUM_ITERS = 15;
 
-sycl::event matrix_mul(synergy::queue& q, std::vector<int>& a, std::vector<int>& b, std::vector<int>& c, synergy::frequency freq = 0) {
+void matrix_mul(synergy::queue& q, std::vector<int>& a, std::vector<int>& b, std::vector<int>& c, synergy::frequency freq = 0) {
   sycl::buffer<int, 2> a_buf(a.data(), sycl::range<2>{N, N});
   sycl::buffer<int, 2> b_buf(b.data(), sycl::range<2>{N, N});
   sycl::buffer<int, 2> c_buf(c.data(), sycl::range<2>{N, N});
 
-  sycl::event e = q.submit(0, freq, [&](sycl::handler& h) {
-    sycl::accessor a_acc{a_buf, h, sycl::read_only};
-    sycl::accessor b_acc{b_buf, h, sycl::read_only};
-    sycl::accessor c_acc{c_buf, h, sycl::read_write};
+  for (int _ = 0; _ < NUM_ITERS; _++) {
+    sycl::event e = q.submit(0, freq, [&](sycl::handler& h) {
+      sycl::accessor a_acc{a_buf, h, sycl::read_only};
+      sycl::accessor b_acc{b_buf, h, sycl::read_only};
+      sycl::accessor c_acc{c_buf, h, sycl::read_write};
 
-    sycl::range<2> grid{N, N};
-    sycl::range<2> block{N < 32 ? N : 32, N < 32 ? N : 32};
+      sycl::range<2> grid{N, N};
+      sycl::range<2> block{N < 32 ? N : 32, N < 32 ? N : 32};
 
-    h.parallel_for<class mat_mul>(sycl::nd_range<2>(grid, block), [=](sycl::nd_item<2> idx) {
-      int i = idx.get_global_id(0);
-      int j = idx.get_global_id(1);
+      h.parallel_for<class mat_mul>(sycl::nd_range<2>(grid, block), [=](sycl::nd_item<2> idx) {
+        int i = idx.get_global_id(0);
+        int j = idx.get_global_id(1);
 
-      c_acc[i][j] = 0.0f;
-      for (size_t k = 0; k < N; k++) {
-        c_acc[i][j] += a_acc[i][k] * b_acc[k][j];
-      }
+        c_acc[i][j] = 0.0f;
+        for (size_t k = 0; k < N; k++) {
+          c_acc[i][j] += a_acc[i][k] * b_acc[k][j];
+        }
+      });
     });
-  });
-  
-  return e;
+  }
 }
 
 template<typename T>
 void print_metrics(std::vector<T> values, std::string label, std::string unit = "") {
+
   T avg = std::accumulate(values.begin(), values.end(), 0.0) / values.size();
   // stdev
   T accum = 0.0;
@@ -73,8 +76,8 @@ int main(int argc, char** argv) {
   std::vector<synergy::energy> host_consumptions;
   for (int i = 0; i < NUM_RUNS + 1; i++) {
     auto start = std::chrono::high_resolution_clock::now();
-    auto e = matrix_mul(q, a, b, c, freq);
-    e.wait();
+    matrix_mul(q, a, b, c, freq);
+    q.wait_and_throw();
     auto end = std::chrono::high_resolution_clock::now();
     double duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
     device_times.push_back(duration);
