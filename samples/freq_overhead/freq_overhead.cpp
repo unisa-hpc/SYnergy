9e305ee75430426791a000e225e610e5687a9ba4
antonio-decaro <antonio.decaro99@outlook.it>
2024-02-02 18:52:36 +0100

    Add sobel kernel for image processing

--- samples/freq_overhead/freq_overhead.cpp
+++ samples/freq_overhead/freq_overhead.cpp
@@ -9,6 +9,7 @@ constexpr size_t NUM_ITERS = 15;
 constexpr size_t SAMPLING_TIME = 2000; // milliseconds
 
 class mat_mul_kernel;
+class sobel_kernel;
 
 /**
  * @brief Sample the energy consumption of the host
@@ -23,15 +24,12 @@ synergy::energy sample_energy_consumption(unsigned int sampling_time) {
 }
 
 void matrix_mul(synergy::queue& q, 
-              std::vector<int>& a, 
-              std::vector<int>& b, 
-              std::vector<int>& c, 
+              sycl::buffer<int, 2>& a_buf,
+              sycl::buffer<int, 2>& b_buf,
+              sycl::buffer<int, 2>& c_buf,
               synergy::frequency freq = 0,
               size_t num_iters = NUM_ITERS,
               size_t skip_factor = 1 ) {
-  sycl::buffer<int, 2> a_buf(a.data(), sycl::range<2>{N, N});
-  sycl::buffer<int, 2> b_buf(b.data(), sycl::range<2>{N, N});
-  sycl::buffer<int, 2> c_buf(c.data(), sycl::range<2>{N, N});
 
   for (int it = 0; it < num_iters; it++) {
     synergy::frequency change_freq = (it % skip_factor == 0) ? freq : 0; // if the freq is zero no frequency change call will be invoked
@@ -54,6 +52,68 @@ void matrix_mul(synergy::queue& q,
       });
     });
   }
+}
+
+sycl::event sobel(sycl::queue& q, 
+                  sycl::buffer<sycl::float4, 2>& input_buf, 
+                  sycl::buffer<sycl::float4, 2>& output_buf, 
+                  size_t size,
+                  synergy::frequency freq = 0,
+                  size_t num_iters = NUM_ITERS,
+                  size_t skip_factor = 1) {
+  return q.submit([&](sycl::handler& cgh) {
+    auto in = input_buf.get_access<sycl::access::mode::read>(cgh);
+    auto out = output_buf.get_access<sycl::access::mode::discard_write>(cgh);
+    sycl::range<2> ndrange{size, size};
+
+    // Sobel kernel 3x3
+    const float kernel[] = {1, 0, -1, 2, 0, -2, 1, 0, -1};
+
+    cgh.parallel_for<sobel_kernel>(
+        ndrange, [in, out, kernel, size_ = size](sycl::id<2> gid) {
+      int x = gid[0];
+      int y = gid[1];
+
+        sycl::float4 Gx = sycl::float4(0, 0, 0, 0);
+        sycl::float4 Gy = sycl::float4(0, 0, 0, 0);
+        const int radius = 3;
+
+        // constant-size loops in [0,1,2]
+        for(int x_shift = 0; x_shift < 3; x_shift++) {
+          for(int y_shift = 0; y_shift < 3; y_shift++) {
+            // sample position
+            uint xs = x + x_shift - 1; // [x-1,x,x+1]
+            uint ys = y + y_shift - 1; // [y-1,y,y+1]
+            // for the same pixel, convolution is always 0
+            if(x == xs && y == ys)
+              continue;
+            // boundary check
+            if(xs < 0 || xs >= size_ || ys < 0 || ys >= size_)
+              continue;
+
+            // sample color
+            sycl::float4 sample = in[{xs, ys}];
+
+            // convolution calculation
+            int offset_x = x_shift + y_shift * radius;
+            int offset_y = y_shift + x_shift * radius;
+
+            float conv_x = kernel[offset_x];
+            sycl::float4 conv4_x = sycl::float4(conv_x);
+            Gx += conv4_x * sample;
+
+            float conv_y = kernel[offset_y];
+            sycl::float4 conv4_y = sycl::float4(conv_y);
+            Gy += conv4_y * sample;
+          }
+        }
+        // taking root of sums of squares of Gx and Gy
+        sycl::float4 color = hypot(Gx, Gy);
+        sycl::float4 minval = sycl::float4(0.0, 0.0, 0.0, 0.0);
+        sycl::float4 maxval = sycl::float4(1.0, 1.0, 1.0, 1.0);
+        out[gid] = clamp(color, minval, maxval);
+    });
+  });
 }
 
 template<typename T>
@@ -101,6 +161,16 @@ int main(int argc, char** argv) {
   std::vector<int> b(N * N, 1);
   std::vector<int> c(N * N, 0);
 
+  sycl::buffer<int, 2> a_buf(a.data(), sycl::range<2>{N, N});
+  sycl::buffer<int, 2> b_buf(b.data(), sycl::range<2>{N, N});
+  sycl::buffer<int, 2> c_buf(c.data(), sycl::range<2>{N, N});
+
+  std::vector<sycl::float4> input;
+  std::vector<sycl::float4> output;
+
+  sycl::buffer<sycl::float4, 2> input_buf(input.data(), sycl::range<2>{N, N});
+  sycl::buffer<sycl::float4, 2> output_buf(output.data(), sycl::range<2>{N, N});
+
   auto starting_energy = sample_energy_consumption(SAMPLING_TIME);
 
   synergy::queue q {sycl::gpu_selector_v, sycl::property_list{sycl::property::queue::enable_profiling{}, sycl::property::queue::in_order{}}};
@@ -110,7 +180,8 @@ int main(int argc, char** argv) {
   std::vector<synergy::energy> host_consumptions;
   for (int i = 0; i < NUM_RUNS + 1; i++) {
     auto start = std::chrono::high_resolution_clock::now();
-    matrix_mul(q, a, b, c, freq, num_iters, skip_factor);
+    matrix_mul(q, a_buf, b_buf, c_buf, freq, num_iters, skip_factor);
+    sobel(q, input_buf, output_buf, N, freq, num_iters, skip_factor);
     q.wait_and_throw();
     auto end = std::chrono::high_resolution_clock::now();
     double duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
