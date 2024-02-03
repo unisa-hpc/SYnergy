3ee745ba63386bcb80ab8cdf749c6ca20c0828bc
Antonio De Caro <antonio.decaro99@outlook.it>
2024-02-03 23:30:28 +0000

    updated colors in the chart

--- samples/freq_overhead/plot.py
+++ samples/freq_overhead/plot.py
@@ -29,18 +29,18 @@ df['phase_overhead_time'] = data['phase_freq_change_time_overhead_Average']
 df['kernel_total_time'] = data['kernel_total_time_Average']
 df['kernel_overhead_time'] = data['kernel_freq_change_time_overhead_Average']
 
-plt.bar(x - bar_width, df['app_total_time'], width=bar_width, color='blue')
-plt.bar(x - bar_width, df['app_overhead_time'], width=bar_width, bottom=df['app_total_time'], color='orange')
+plt.bar(x - bar_width, df['app_total_time'], width=bar_width, color='royalblue')
+plt.bar(x - bar_width, df['app_overhead_time'], width=bar_width, bottom=df['app_total_time'], color='darkorange')
 
-plt.bar(x, df['phase_total_time'], width=bar_width, color='blue', hatch=PHASE_HATCHES)
-plt.bar(x, df['phase_overhead_time'], width=bar_width, bottom=df['phase_total_time'],  hatch=PHASE_HATCHES, color='orange')
+plt.bar(x, df['phase_total_time'], width=bar_width, color='royalblue', hatch=PHASE_HATCHES)
+plt.bar(x, df['phase_overhead_time'], width=bar_width, bottom=df['phase_total_time'],  hatch=PHASE_HATCHES, color='darkorange')
 
-plt.bar(x + bar_width, df['kernel_total_time'], width=bar_width, color='blue', hatch=KERNEL_HATCHES)
-plt.bar(x + bar_width, df['kernel_overhead_time'], width=bar_width, bottom=df['kernel_total_time'],  hatch=KERNEL_HATCHES, color='orange')
+plt.bar(x + bar_width, df['kernel_total_time'], width=bar_width, color='royalblue', hatch=KERNEL_HATCHES)
+plt.bar(x + bar_width, df['kernel_overhead_time'], width=bar_width, bottom=df['kernel_total_time'],  hatch=KERNEL_HATCHES, color='darkorange')
 
 
-legend = [Patch(facecolor='blue', label='Computation Time'), 
-          Patch(facecolor='orange', label='Frequency Change Time'),
+legend = [Patch(facecolor='royalblue', label='Computation Time'), 
+          Patch(facecolor='darkorange', label='Frequency Change Time'),
           Patch(facecolor='none', edgecolor='k', label='Per App Frequency Change'),
           Patch(facecolor='none', edgecolor='k', hatch=PHASE_HATCHES + ".", label='Per Phase Frequency Change'),
           Patch(facecolor='none', edgecolor='k', hatch=KERNEL_HATCHES, label='Per Kernel Frequency Change'),
@@ -67,18 +67,18 @@ df['phase_overhead_energy'] = data['phase_freq_change_device_energy_overhead_Ave
 df['kernel_device_energy'] = data['kernel_device_energy_Average'] + data['kernel_host_energy_Average']
 df['kernel_overhead_energy'] = data['kernel_freq_change_device_energy_overhead_Average'] + data['kernel_freq_change_host_energy_overhead_Average']
 
-plt.bar(x - bar_width, df['app_device_energy'], width=bar_width, color='blue')
-plt.bar(x - bar_width, df['app_overhead_energy'], width=bar_width, bottom=df['app_device_energy'], color='orange')
+plt.bar(x - bar_width, df['app_device_energy'], width=bar_width, color='royalblue')
+plt.bar(x - bar_width, df['app_overhead_energy'], width=bar_width, bottom=df['app_device_energy'], color='darkorange')
 
-plt.bar(x, df['phase_device_energy'], width=bar_width, color='blue', hatch=PHASE_HATCHES)
-plt.bar(x, df['phase_overhead_energy'], width=bar_width, bottom=df['phase_device_energy'],  hatch=PHASE_HATCHES, color='orange')
+plt.bar(x, df['phase_device_energy'], width=bar_width, color='royalblue', hatch=PHASE_HATCHES)
+plt.bar(x, df['phase_overhead_energy'], width=bar_width, bottom=df['phase_device_energy'],  hatch=PHASE_HATCHES, color='darkorange')
 
-plt.bar(x + bar_width, df['kernel_device_energy'], width=bar_width, color='blue', hatch=KERNEL_HATCHES)
-plt.bar(x + bar_width, df['kernel_overhead_energy'], width=bar_width, bottom=df['kernel_device_energy'],  hatch=KERNEL_HATCHES, color='orange')
+plt.bar(x + bar_width, df['kernel_device_energy'], width=bar_width, color='royalblue', hatch=KERNEL_HATCHES)
+plt.bar(x + bar_width, df['kernel_overhead_energy'], width=bar_width, bottom=df['kernel_device_energy'],  hatch=KERNEL_HATCHES, color='darkorange')
 
 
-legend = [Patch(facecolor='blue', label='Computation Energy'), 
-          Patch(facecolor='orange', label='Frequency Change Energy'),
+legend = [Patch(facecolor='royalblue', label='Computation Energy'), 
+          Patch(facecolor='darkorange', label='Frequency Change Energy'),
           Patch(facecolor='none', edgecolor='k', label='Per App Frequency Change'),
           Patch(facecolor='none', edgecolor='k', hatch=PHASE_HATCHES + ".", label='Per Phase Frequency Change'),
           Patch(facecolor='none', edgecolor='k', hatch=KERNEL_HATCHES, label='Per Kernel Frequency Change'),
