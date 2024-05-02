38c5d715e91d6ebe3fd26cd0b2690fdfda614824
lorenzo-carpentieri <lorenzocarpentieri34@gmail.com>
2024-05-02 18:36:16 +0000

    Fixed sycl properties  in queue constructor. Now the synergy queue is transparently built with in_order and enable profiling properties when kernel profiling is on

--- include/queue.hpp
+++ include/queue.hpp
@@ -172,19 +172,21 @@ private:
 #ifdef SYNERGY_KERNEL_PROFILING
     // check if it has some standard property
     if constexpr (
-        (std::is_same_v<sycl::property::queue::enable_profiling&, Args> || ...) ||
-        (std::is_same_v<sycl::property::queue::enable_profiling, Args> || ...) ||
-        (std::is_same_v<sycl::property::queue::in_order&, Args> || ...) ||
-        (std::is_same_v<sycl::property::queue::in_order, Args> || ...) ||
-        (std::is_same_v<sycl::property_list&, Args> || ...) ||
-        (std::is_same_v<sycl::property_list, Args> || ...)
-    )
-      return sycl::queue(std::forward<Args>(args)..., sycl::property_list{sycl::property::queue::enable_profiling{}});
+        (std::is_same_v<sycl::property::queue::enable_profiling, std::remove_reference_t<Args>> || ...) ||
+        (std::is_same_v<sycl::property::queue::in_order, std::remove_reference_t<Args>> || ...) ||
+        (std::is_same_v<sycl::property_list, std::remove_reference_t<Args>> || ...)
+    ){
+      std::cout<< "Kernel-profiling: constexpr if" <<std::endl; 
+      sycl::queue fake_queue = sycl::queue(std::forward<Args>(args)...);
+      return sycl::queue(fake_queue.get_device(), sycl::property_list{sycl::property::queue::enable_profiling{}, sycl::property::queue::in_order {}});
+    }
+    
     else {
+      std::cout<< "Kernel-profiling: constexpr else" <<std::endl; 
       return sycl::queue(std::forward<Args>(args)..., sycl::property_list{sycl::property::queue::enable_profiling{}, sycl::property::queue::in_order {}});
     }
 #else
-    return sycl::queue(std::forward<Args>(args)..., sycl::property_list{sycl::property::queue::enable_profiling{}});
+    return sycl::queue(std::forward<Args>(args)..., sycl::property_list{sycl::property::queue::enable_profiling{}, sycl::property::queue::in_order{}});
 #endif
   }
 
