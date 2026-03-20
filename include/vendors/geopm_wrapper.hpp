0ae3b0c7a60c1f336fead3b02d09fc97761d76ca
lorenzo-carpentieri <lorenzocarpentieri34@gmail.com>
2026-03-20 14:27:49 +0000

    [GEOPM_BACKEND]: fixed core frequency scaling on Intel GPUs in COMPOSITE mode. In COMPOSITE mode when the synergy::queue is built with a root device composed of multiple subdevices the set_core_frequency function will change the frequency of all subdevices. Differenty if the synergy::queue is built with a subdevice only the frequnecy of the target  subdevice will be changed.

--- include/vendors/geopm_wrapper.hpp
+++ include/vendors/geopm_wrapper.hpp
@@ -13,11 +13,13 @@
 
 namespace g = geopm;
 
+
 namespace synergy {
 
 namespace detail {
 
 namespace management {
+  const int NUM_TILES = 2; // We assume two tile in composite mode. TODO: handle more then 2 tiles.
   enum class synergy_signal {
     GET_POWER,
     GET_ENERGY,
@@ -28,6 +30,9 @@ namespace management {
     SET_MAX_CORE_FREQ
 
   };
+
+ 
+
   // Map synergy signal to geopm signal according to the GEOPM GPU domain
   inline std::string geopm_signal_name(synergy_signal signal, geopm_domain_e domain){
       // domain = GEOPM_DOMAIN_GPU or GEOPM_DOMAIN_GPU_CHIP
@@ -48,10 +53,12 @@ namespace management {
               throw std::logic_error("Unknown synergy_signal");
       }
   }
+  
   struct geopm_device_handle {
     unsigned int id;
     geopm_domain_e domain;  // GEOPM_DOMAIN_GPU or GEOPM_DOMAIN_GPU_CHIP
   };
+  
   struct geopm {
     static constexpr std::string_view name = "GEOPM";
     static constexpr unsigned int max_frequencies = 256;
@@ -61,7 +68,27 @@ namespace management {
     using return_type = unsigned int;
     static constexpr return_type return_success = 0;
   };
+
+  /********* Geopm read and geopm write *********/
+  double geopm_read(synergy_signal synergy_signal, geopm_domain_e domain, geopm::device_identifier id){
+    return g::platform_io().read_signal(
+      geopm_signal_name(synergy_signal, domain), 
+      static_cast<int>(domain), 
+      id);
+  }
+ 
+  void geopm_write(synergy_signal synergy_signal, geopm_domain_e domain, geopm::device_identifier id, double val){
+    g::platform_io().write_control(
+          geopm_signal_name(synergy_signal, domain), 
+          domain, 
+          id, 
+          val);
+  }
+  /******************************** */
+
 } // namespace management
+ 
+
 
 template <>
 struct vendor_traits<management::geopm> {
@@ -91,23 +118,23 @@ public:
   }
 
   //TODO: check if there is a more accurate command in geopm for power
-  inline power get_power_usage(geopm::device_handle handle) const {
+  inline synergy::power get_power_usage(geopm::device_handle handle) const {
     std::string debug_msg = "get_power_usage -> Vendor id: " + std::to_string(handle.id) + " domain: " + std::to_string(static_cast<int>(handle.domain));
     synergy::log::synergy_log(synergy::log::LogLevel::Debug, debug_msg);
     
-    unsigned int power = g::platform_io().read_signal(
-        synergy::detail::management::geopm_signal_name(synergy::detail::management::synergy_signal::GET_POWER, handle.domain), 
-        static_cast<int>(handle.domain), 
+    double power_val = management::geopm_read(
+        management::synergy_signal::GET_POWER, 
+        handle.domain, 
         handle.id);
-    synergy::log::synergy_log(synergy::log::LogLevel::Debug, "Power read: " + std::to_string(power));
-    return power * 1e6; // from W to uW
+    synergy::log::synergy_log(synergy::log::LogLevel::Debug, "Power read: " + std::to_string(power_val));
+    return static_cast<synergy::power>(power_val * 1e6); // from W to uW
   }
 
   inline energy get_energy_usage(geopm::device_handle handle) const {
-    unsigned int energy = g::platform_io().read_signal(
-        synergy::detail::management::geopm_signal_name(synergy::detail::management::synergy_signal::GET_ENERGY, handle.domain), 
-        handle.domain,
-        handle.id);
+    unsigned int energy = management::geopm_read(management::synergy_signal::GET_ENERGY,
+      handle.domain, 
+      handle.id
+    );
     return energy * 1e6; // from J to uJ
   }
 
@@ -132,27 +159,34 @@ public:
     std::string debug_msg = "get_core_frequency -> Vendor id: " + std::to_string(handle.id) + " domain: " + std::to_string(static_cast<int>(handle.domain));
     synergy::log::synergy_log(synergy::log::LogLevel::Debug, debug_msg);
 
-    /* In composite mode we can use a GPU with two tile in that case we assume that the frequency is the mean between the two freq */
+    /* 
+      In FLAT mode SYnergy return the core frequnecy of the selected tile. Differently in COMPOSITE mode we can have two scenarios:
+       1. The device used by the syenergy::queue is a subdevice of the root. In that case SYnergy automatically set the GEOPM domain to FLAT,
+          so that each subdevice is managed has a single independent device.
+       2. The device use byt the synergy::queue is a root device composed of two tiles. 
+          In that case the GEOPM domain is set to composite but we have to query the core frquency of both tile: for now we return a single value that 
+          is the mean of the core freq.
+          TODO: return an std::vector that contains the frequnecy of each tile.
+    */
     if ( handle.domain == GEOPM_DOMAIN_GPU_CHIP){
-      frequency freq =  g::platform_io().read_signal(
-        synergy::detail::management::geopm_signal_name(synergy::detail::management::synergy_signal::GET_CORE_FREQUENCY, handle.domain), 
-        GEOPM_DOMAIN_GPU_CHIP, // Domain here must be GPU_CHIP since each tile can have a diffrent frequency 
+      frequency freq = management::geopm_read(
+        management::synergy_signal::GET_CORE_FREQUENCY,
+        handle.domain, 
         handle.id) * 1e-6;
         
-        synergy::log::synergy_log(synergy::log::LogLevel::Debug, "Core frequency: " + std::to_string(freq) + " MHz");
-      
+        synergy::log::synergy_log(synergy::log::LogLevel::Debug, "Core frequency: " + std::to_string(freq) + " MHz");      
         return freq;
     }
     else{
-        frequency freq_0 =  g::platform_io().read_signal(
-        synergy::detail::management::geopm_signal_name(synergy::detail::management::synergy_signal::GET_CORE_FREQUENCY, handle.domain), 
+        frequency freq_0 = management::geopm_read(
+        management::synergy_signal::GET_CORE_FREQUENCY, 
         GEOPM_DOMAIN_GPU_CHIP, // Domain here must be GPU_CHIP since each tile can have a diffrent frequency 
-        handle.id) * 1e-6;
+        handle.id * management::NUM_TILES) * 1e-6;
         
-        frequency freq_1 =  g::platform_io().read_signal(
-        synergy::detail::management::geopm_signal_name(synergy::detail::management::synergy_signal::GET_CORE_FREQUENCY, handle.domain), 
-        GEOPM_DOMAIN_GPU_CHIP, // Domain here must be GPU_CHIP since each tile can have a diffrent frequency 
-        handle.id+1) * 1e-6;
+        frequency freq_1 = management::geopm_read(
+          management::synergy_signal::GET_CORE_FREQUENCY, 
+          GEOPM_DOMAIN_GPU_CHIP, // Domain here must be GPU_CHIP since each tile can have a diffrent frequency 
+          (handle.id* management::NUM_TILES)+1) * 1e-6;
         
       return (freq_0 + freq_1) / 2;
     }
@@ -165,55 +199,126 @@ public:
   inline temperature_t get_temperature(geopm::device_handle handle) const{
     temperature_t temperature=0;
     if(handle.domain == GEOPM_DOMAIN_GPU){
-      temperature +=  g::platform_io().read_signal(
-        synergy::detail::management::geopm_signal_name(synergy::detail::management::synergy_signal::GET_TEMPERATURE, handle.domain), 
+      temperature +=  management::geopm_read(
+        management::synergy_signal::GET_TEMPERATURE, 
         GEOPM_DOMAIN_GPU_CHIP, // Domain here must be GPU_CHIP since each tile can have a diffrent frequency 
-        handle.id);
+        handle.id * management::NUM_TILES);
         temperature +=  g::platform_io().read_signal(
-        synergy::detail::management::geopm_signal_name(synergy::detail::management::synergy_signal::GET_TEMPERATURE, handle.domain), 
+        management::geopm_signal_name(management::synergy_signal::GET_TEMPERATURE, handle.domain), 
         GEOPM_DOMAIN_GPU_CHIP, // Domain here must be GPU_CHIP since each tile can have a diffrent frequency 
-        handle.id+1);  
+        handle.id * management::NUM_TILES + 1);  
     }else{
-        temperature =  g::platform_io().read_signal(
-          synergy::detail::management::geopm_signal_name(synergy::detail::management::synergy_signal::GET_TEMPERATURE, handle.domain), 
-          GEOPM_DOMAIN_GPU_CHIP, // Domain here must be GPU_CHIP since each tile can have a diffrent frequency 
+        temperature =  management::geopm_read(
+          management::synergy_signal::GET_TEMPERATURE, 
+          handle.domain, // handle.domain is GPU_CHIP 
           handle.id);
       }
       return temperature;
   }
+  
   inline void set_core_frequency(geopm::device_handle handle, frequency target) const {
     std::string debug_msg = "set_core_frequency -> Vendor id: " + std::to_string(handle.id) + " domain: " + std::to_string(static_cast<int>(handle.domain));
     synergy::log::synergy_log(synergy::log::LogLevel::Debug, debug_msg);
-    
     // MIN_CORE_FREQ should always be less equal then MAX_CORE_FREQ. This check is to avoid wrong min max core frequency settings.
     frequency curr_freq = get_core_frequency(handle);
     synergy::log::synergy_log(synergy::log::LogLevel::Debug, "Current freq: " + std::to_string(curr_freq) + " MHz");
+    /* 
+        In SYnergy with Intel COMPOSITE mode we can have two scenarion:
+        1. The device provided to the synergy queue is the root device composes of two tiles. In that case SYnergy will set the GEOPM domain to composite.
+           And when the set_core frequency function is called SYnergy set the frequency of both tile. Root device i will have tile with index i*2, i*2+1.
+        2. The device provided to the SYnergy queue is a subdevice of the root. In that case we set the GEOPM domain to flat. So that we can mange each
+           subdevice independently.
+    */
+    
+    if (target >= curr_freq){ // handle min/max freq control
+      if(handle.domain == geopm_domain_e::GEOPM_DOMAIN_GPU_CHIP){ // handle FLAT mode and Composite mode with subdeiveces
+        management::geopm_write(
+          management::synergy_signal::SET_MAX_CORE_FREQ,
+          handle.domain,
+          handle.id, 
+          target * 1e6);
+        management::geopm_write(
+          management::synergy_signal::SET_MIN_CORE_FREQ, 
+          handle.domain, 
+          handle.id, 
+          target * 1e6);
+      }
+      else{ // handle composite mode with synergy::queue built with a root device
+        /***************** First tile freq change **********/
+        management::geopm_write(
+          management::synergy_signal::SET_MAX_CORE_FREQ, 
+          GEOPM_DOMAIN_GPU_CHIP, // here we need GPU_CHIP domain since we have to change frequency for both tile 
+          handle.id * management::NUM_TILES, 
+          target * 1e6);
 
-    if (target >= curr_freq){
-      g::platform_io().write_control(
-        synergy::detail::management::geopm_signal_name(synergy::detail::management::synergy_signal::SET_MAX_CORE_FREQ, handle.domain), 
-        handle.domain, 
-        handle.id, 
-        target * 1e6);
-      g::platform_io().write_control(
-        synergy::detail::management::geopm_signal_name(synergy::detail::management::synergy_signal::SET_MIN_CORE_FREQ, handle.domain), 
-        handle.domain, 
-        handle.id, 
-        target * 1e6);
-    }
-    else{
-      g::platform_io().write_control(
-        synergy::detail::management::geopm_signal_name(synergy::detail::management::synergy_signal::SET_MIN_CORE_FREQ, handle.domain), 
-        handle.domain, 
-        handle.id, 
-        target * 1e6);
+        management::geopm_write(
+            management::synergy_signal::SET_MIN_CORE_FREQ,
+            GEOPM_DOMAIN_GPU_CHIP,
+            handle.id * management::NUM_TILES, 
+            target * 1e6);
+        /******************************************************/
 
-      g::platform_io().write_control(
-        synergy::detail::management::geopm_signal_name(synergy::detail::management::synergy_signal::SET_MAX_CORE_FREQ, handle.domain), 
-        handle.domain, 
-        handle.id, 
-        target * 1e6);
+        /***************** Second tile freq change **********/
+        management::geopm_write(
+          management::synergy_signal::SET_MAX_CORE_FREQ, 
+          GEOPM_DOMAIN_GPU_CHIP, // here we need GPU_CHIP domain since we have to change frequency for both tile 
+          handle.id * management::NUM_TILES + 1, 
+          target * 1e6);
+
+        management::geopm_write(
+            management::synergy_signal::SET_MIN_CORE_FREQ,
+            GEOPM_DOMAIN_GPU_CHIP,
+            handle.id * management::NUM_TILES + 1, 
+            target * 1e6);
+            /******************************************************/
+      }
     }
+    else{  // handl min/max freq control
+      if(handle.domain == geopm_domain_e::GEOPM_DOMAIN_GPU_CHIP){ // handle FLAT mode and Composite mode with subdeiveces
+
+        management::geopm_write(
+          management::synergy_signal::SET_MIN_CORE_FREQ, 
+          handle.domain, 
+          handle.id, 
+          target * 1e6);
+
+        management::geopm_write(
+          management::synergy_signal::SET_MAX_CORE_FREQ, 
+          handle.domain, 
+          handle.id, 
+          target * 1e6);
+    
+        }
+        else{ // handle COMPOSITE mode with root device
+          /********** First tile freq change ***********/
+          management::geopm_write(
+            management::synergy_signal::SET_MIN_CORE_FREQ, 
+            handle.domain, 
+            handle.id * management::NUM_TILES, 
+            target * 1e6);
+
+          management::geopm_write(
+            management::synergy_signal::SET_MAX_CORE_FREQ, 
+            handle.domain, 
+            handle.id * management::NUM_TILES, 
+            target * 1e6);
+          /**********************************************/
+          /********** Second tile freq change ***********/
+          management::geopm_write(
+            management::synergy_signal::SET_MIN_CORE_FREQ, 
+            handle.domain, 
+            handle.id * management::NUM_TILES + 1, 
+            target * 1e6);
+
+          management::geopm_write(
+            management::synergy_signal::SET_MAX_CORE_FREQ, 
+            handle.domain, 
+            handle.id * management::NUM_TILES + 1, 
+            target * 1e6);
+            /**********************************************/
+        }
+      }
+
     return ;
   }
 
